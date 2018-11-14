2d3fda91f70f8aaab6194c8f97a51ea164765f25
Fedor Savinykh <fesander@mail.ru>
2018-11-14 07:16:35 +0300

    I have programm which returns menu

--- Lesson_2/menu.js
+++ Lesson_2/menu.js
@@ -1,17 +1,95 @@
-let script = document.createElement('script');
-script.src = '../Lesson_1/menu.js';
-document.getElementsByTagName('head')[0].appendChild(script);
-alert('script loaded');
+function Container() {
+  this.id = 'container';
+  this.className = 'container';
+
+  this.render = function () {
+      let div = document.createElement('div');
+      div.id = this.id;
+      div.className = this.className;
+
+      return div;
+  };
+
+  this.remove = function () {
+      document.getElementById(this.id).remove();
+  }
+}
+
+function Menu(id, className, items) {
+  Container.call(this);
+  this.id = id;
+  this.className = className;
+  this.items = items;
+
+  this.render = function () {
+      let ul = document.createElement('ul');
+      ul.className = this.className;
+      ul.id = this.id;
+      
+      for (let item in items) {
+        if (this.items[item] instanceof MenuItem || this.items[item] instanceof Submenu) {
+              ul.appendChild(this.items[item].render());
+          }
+      }
+
+      return ul;
+  }
+}
+
+function Submenu(link, label, id, menu) {
+    Menu.call(this);
+    this.link = link;
+    this.label = label;
+    this.id = id;
+    this.menu = menu;
+
+    this.render = function () {
+        let li = document.createElement('li');
+        li.id = this.id;
+        let a = document.createElement('a');
+        a.href = this.link;
+        a.textContent = this.label;
+        li.appendChild(a);
+        let ul = document.createElement('ul');
+
+        for (let i = 0; i < this.menu.length; i++) {
+            if (this.menu[i] instanceof MenuItem ) {
+                ul.appendChild(this.menu[i].render());
+            }
+        }
+        li.appendChild(ul);
+        return li;
+    }
+}
+
+function MenuItem(my_id, link, label) {
+    Container.call(this);
+    this.id = my_id;
+    this.link = link;
+    this.label = label;
+
+    this.render = function () {
+        let li = document.createElement('li');
+        li.id = this.id;
+        let a = document.createElement('a');
+        a.href = this.link;
+        a.textContent = this.label;
+        li.appendChild(a);
+
+        return li;
+    }
+}
 
 console.log("The second homework begins");
 
 let my_items;
 function fullMenuContent(xhr) {
-    my_items = [];
+    my_items = {};
 
     if(xhr.readyState === 4) {
         if(xhr.status === 0) {
             let items = JSON.parse(xhr.responseText);
+            console.log(items);
 
             for(let  i = 0; i < items.menu_items.length; i++)
                 my_items[i] = new MenuItem(items.menu_items[i].id, items.menu_items[i].href, items.menu_items[i].title);
@@ -29,6 +107,16 @@ if (window.XMLHttpRequest) {
     try {
         xhr = new ActiveXObject('Msxml2.XMLHTTP');
     } catch(e) {
-        console.log("Error");
+        try {
+            shr = new ActiveXObject('Microsoft.XMLHTTP');
+        } catch(e) {}
     }
 }
+
+if (!xhr) {
+    alert("Error: It is not possible to create xhr");
+}
+
+xhr.onreadystatechange = function() {fullMenuContent(xhr)};
+xhr.open('GET', "menu.json", true);
+xhr.send();
