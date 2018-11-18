f200b164d18cf1aec010fae0eb6abd2e28fed82c
Fedor <fesander@mail.ru>
2018-11-18 15:56:08 +0300

    Update of the econd task with Bootstrap

---
+++ Lesson_2/js/container.js
@@ -0,0 +1,213 @@
+function Container() {
+    this.id = 'container';
+    this.className = 'container';
+
+    this.render = function () {
+        let div = document.createElement('div');
+        div.id = this.id;
+        div.className = this.className;
+
+        return div;
+    };
+
+    this.remove = function () {
+        document.getElementById(this.id).remove();
+    }
+}
+
+function Menu(id, className, items) {
+    Container.call(this);
+    this.id = id;
+    this.className = className;
+    this.items = items;
+
+    this.render = function () {
+        let ul = document.createElement('ul');
+        ul.className = this.className;
+        ul.id = this.id;
+
+        for (let item in items) {
+            if (this.items[item] instanceof MenuItem || this.items[item] instanceof Submenu) {
+                ul.appendChild(this.items[item].render());
+            }
+        }
+
+        return ul;
+    }
+}
+
+function Submenu(id, my_class, link, label, menu) {
+    Menu.call(this);
+    this.link = link;
+    this.label = label;
+    this.id = id;
+    this.menu = menu;
+    this.my_class = my_class;
+
+    this.render = function () {
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+        a.href = this.link;
+        a.id = this.id;
+        a.className = this.my_class;
+        a.textContent = this.label;
+        a.setAttribute("data-toggle", "dropdown");
+        li.appendChild(a);
+        let ul = document.createElement('ul');
+        ul.className = "dropdown-menu";
+
+        for (let item in menu) {
+            if (this.menu[item] instanceof MenuItem || this.menu[item] instanceof Submenu) {
+                ul.appendChild(this.menu[item].render());
+            }
+        }
+
+        li.appendChild(ul);
+        return li;
+    }
+}
+
+function MenuItem(my_id, my_class, link, label) {
+    Container.call(this);
+    this.id = my_id;
+    this.className = my_class;
+    this.link = link;
+    this.label = label;
+
+    this.render = function () {
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+        a.href = this.link;
+        a.id = this.id;
+        a.className = this.className;
+        a.textContent = this.label;
+        a.setAttribute("data-toggle", "tab");
+        li.appendChild(a);
+
+        return li;
+    }
+
+    this.renderContentArea = function() {
+        let div = document.createElement('div');
+        div.className = "tab-pane fade";
+        if (isNaN(this.className.match("active")))
+            div.className += "active show";
+        div.setAttribute("role", "tabpanel");
+        div.setAttribute("aria-labelledby", this.id);
+        let regular = "[a-zA-Z]+";
+        div.id = this.link.match(regular);
+        return div;
+    }
+}
+
+function Content(my_id, my_class, item) {
+    Container.call(this);
+    this.id = my_id;
+    this.className = my_class;
+    this.items = item;
+
+    this.render = function () {
+        let div = document.createElement('div');
+        div.className = this.className;
+        div.id = this.id;
+
+        // for (let item in items) {
+        //     if (this.items[item] instanceof MenuItem) {
+        //         div.appendChild(this.items[item].renderContentArea());
+        //     }
+        // }
+
+        return div;
+    }
+}
+
+function TextContent(items) {
+    Container.call(this);
+    this.items = items;
+
+    this.render = function () {
+        let div = document.createElement('div');
+ //       div.classList = "container";
+
+        for (let item in items) {
+            if (this.items[item]){
+                let p = document.createElement('p');
+                p.textContent = this.items[item].paragraph;
+                p.classList = "paragraphs lead";
+                div.appendChild(p);
+            }
+        }
+        let button = document.createElement('button');
+        button.classList = "btn btn-secondary btn-lg";
+        button.textContent = "Форматтирование";
+        button.setAttribute("type", "button");
+        button.id = "formatting";
+        div.appendChild(button);
+        return div;
+    }
+}
+
+function PromoContainer(className, items) {
+    Container.call(this);
+    this.className = className;
+    this.items = items;
+
+    this.render = function () {
+        let div = document.createElement('div');
+        div.className = this.className;
+        div.appendChild(this.items.render());
+
+        return div;
+    }
+}
+
+function Image(my_link) {
+    Container.call(this);
+    this.link = my_link;
+    this.id = "";
+    this.className = "";
+
+    this.render = function () {
+        let img = document.createElement('img');
+        img.src = this.link;
+        img.className += 'smallImage';
+        return img;
+    }
+}
+
+function Gallery(className, items) {
+    Container.call(this);
+    this.className = className;
+    this.items = items;
+
+    this.render = function () {
+        let div = document.createElement('div');
+        div.className = this.className;
+
+        for (let item in items) {
+            if (this.items[item] instanceof GalleryItem) {
+                div.appendChild(this.items[item].render());
+            }
+        }
+
+        return div;
+    }
+}
+
+function GalleryItem(my_class, my_link) {
+    Container.call(this);
+    this.class = my_class;
+    this.link = my_link;
+
+    this.render = function () {
+        let a = document.createElement('a');
+        a.className = this.class;
+        a.className += ' smallImage';
+        a.target = '_blank';
+        a.href = this.link;
+        // Вставка стиль прямо в html, это позволяет обдейтить только json файл,
+        // для добавления новых картинок и не трогать файл стилей
+        a.style = "background-image: url('" + this.link + "')";
+        return a;
+    }
+}
--- Lesson_2/js/menu.js
+++ Lesson_2/js/menu.js
@@ -1,95 +1,22 @@
-function Container() {
-  this.id = 'container';
-  this.className = 'container';
-
-  this.render = function () {
-      let div = document.createElement('div');
-      div.id = this.id;
-      div.className = this.className;
-
-      return div;
-  };
-
-  this.remove = function () {
-      document.getElementById(this.id).remove();
-  }
-}
-
-function Menu(id, className, items) {
-  Container.call(this);
-  this.id = id;
-  this.className = className;
-  this.items = items;
-
-  this.render = function () {
-      let ul = document.createElement('ul');
-      ul.className = this.className;
-      ul.id = this.id;
-      
-      for (let item in items) {
-        if (this.items[item] instanceof MenuItem || this.items[item] instanceof Submenu) {
-              ul.appendChild(this.items[item].render());
-          }
-      }
-
-      return ul;
-  }
-}
-
-function Submenu(link, label, id, menu) {
-    Menu.call(this);
-    this.link = link;
-    this.label = label;
-    this.id = id;
-    this.menu = menu;
-
-    this.render = function () {
-        let li = document.createElement('li');
-        li.id = this.id;
-        let a = document.createElement('a');
-        a.href = this.link;
-        a.textContent = this.label;
-        li.appendChild(a);
-        let ul = document.createElement('ul');
-
-        for (let item in menu) {
-            if (this.menu[item] instanceof MenuItem || this.menu[item] instanceof Submenu) {
-                ul.appendChild(this.menu[item].render());
-            }
-        }
-
-        li.appendChild(ul);
-        return li;
-    }
-}
-
-function MenuItem(my_id, link, label) {
-    Container.call(this);
-    this.id = my_id;
-    this.link = link;
-    this.label = label;
-
-    this.render = function () {
-        let li = document.createElement('li');
-        li.id = this.id;
-        let a = document.createElement('a');
-        a.href = this.link;
-        a.textContent = this.label;
-        li.appendChild(a);
-
-        return li;
-    }
-}
-
 // Рекурсивная функция, позволяющая получить любую степень вложенности меню
 function parseJSONtoObject(items) {
     let my_items = {};
 
     for(let  i = 0; i < items.length; i++) {
         if(items[i].items) {
-            my_items[i] = new Submenu(items[i].href, items[i].title, items[i].id, parseJSONtoObject(items[i].items));
+            my_items[i] = new Submenu(items[i].id, items[i].class, items[i].href, items[i].title, parseJSONtoObject(items[i].items));
         } else
-            my_items[i] = new MenuItem(items[i].id, items[i].href, items[i].title);
+            my_items[i] = new MenuItem(items[i].id, items[i].class, items[i].href, items[i].title);
+    }
+
+    return  my_items;
+}
+
+function parseJSONtoGalleryItem(items) {
+    let my_items = {};
+
+    for(let  i = 0; i < items.length; i++) {
+        my_items[i] = new GalleryItem(items[i].class, items[i].href);
     }
 
     return  my_items;
@@ -103,10 +30,161 @@ function fullMenuContent(xhr) {
             let items = JSON.parse(xhr.responseText);
             console.log(items);
 
-            let menu = new Menu('menu', 'menu', parseJSONtoObject(items.menu_items));
+            let menu = new Menu('myTab', 'nav nav-tabs', parseJSONtoObject(items.menu_items));
             let header = document.createElement('header');
             header.appendChild(menu.render());
             document.body.appendChild(header);
+            let content = new Content('myTabContent', 'tab-content', "main_tab");
+            document.body.appendChild(content.render());
+            mainPageContent(xhr);
+
+            let el = document.getElementsByClassName("nav-link");
+            for(let i = 0; i < el.length; i++)
+                if(!el[i].classList.contains("dropdown-toggle"))
+                    el[i].addEventListener("click", modifyText, false);
+            //
+            //
+            // let images = JSON.parse(xhr.responseText);
+            // let a = parseJSONtoGalleryItem(images.image_items);
+            // container = document.getElementById("gallery");
+            // let gallery = new Gallery('container', a);
+            // container.appendChild(gallery.render());
+        }
+    }
+}
+
+function modifyText() {
+    console.log("Current tab = " + this.id);
+    if(document.getElementById("myTabContent"))
+        document.getElementById("myTabContent").remove();
+
+    let content = new Content('myTabContent', 'tab-content', this.id);
+    document.body.appendChild(content.render());
+    switch (this.id) {
+        case "home-tab":
+            mainPageContent(xhr);
+            break;
+        case "catalog-man":
+            break;
+        case "catalog-woman":
+            break;
+        case "gallery-tab":
+            fullGalleryContent(xhr);
+            break;
+        case "sale-tab":
+            saleAvaleble();
+            break;
+        case "contact-tab":
+            break;
+    }
+}
+
+function parseJSONtoParagraphs(items) {
+    let my_items = {};
+
+    for(let  i = 0; i < items.length; i++) {
+        my_items[i] = new GalleryItem(items[i].class, items[i].href);
+    }
+
+    return  my_items;
+}
+
+function mainPageContent(xhr) {
+
+    if(xhr.readyState === 4) {
+        if(xhr.status === 0) {
+            let paragraphs = JSON.parse(xhr.responseText);
+            // text = parseJSONtoParagraphs(paragraphs.text_content);
+            container = document.getElementById("myTabContent");
+            let article = new TextContent(paragraphs.text_content);
+            container.appendChild(article.render());
+            document.getElementById("formatting").addEventListener("click", textFormat, false);
+        }
+    }
+}
+
+function textFormat() {
+//    console.log("Formatting");
+    let items = document.getElementsByClassName("paragraphs");
+    let div = document.createElement('div');
+    for (item in items) {
+        text = items[item].textContent;
+        if (text !== undefined) {
+            console.log(text);
+            let regexp = ("Elon");
+            text = text.replace(regexp, function (free) {
+                let span = document.createElement('span');
+                span.textContent = free;
+                span.classList = "red";
+                return span;
+            });
+        }
+        //       div.classList = "container";
+        let p = document.createElement('p');
+        p.textContent = text;
+        p.classList = "paragraphs lead";
+        div.appendChild(p);
+    }
+    let button = document.createElement('button');
+    button.classList = "btn btn-secondary btn-lg";
+    button.textContent = "Форматтирование";
+    button.setAttribute("type", "button");
+    button.id = "formatting";
+    div.appendChild(button);
+    document.getElementById("myTabContent").remove();
+    let content = new Content('myTabContent', 'tab-content', "main_tab");
+    document.body.appendChild(content.render());
+    content.appendChild(div);
+}
+
+function parseJSONtoGalleryItem(items) {
+    let my_items = {};
+
+    for(let  i = 0; i < items.length; i++) {
+        my_items[i] = new GalleryItem(items[i].class, items[i].href);
+    }
+
+    return  my_items;
+}
+
+function fullGalleryContent(xhr) {
+
+    if(xhr.readyState === 4) {
+        if(xhr.status === 0) {
+            let images = JSON.parse(xhr.responseText);
+            a = parseJSONtoGalleryItem(images.image_items);
+            container = document.getElementById("myTabContent");
+            let gallery = new Gallery('container', a);
+            container.appendChild(gallery.render());
+        }
+    }
+}
+
+function saleAvaleble() {
+// Вызов случайного json файла в зависимости от рандомного числа
+    xhr4.onreadystatechange = function() {errorOrSuccess(xhr4)};
+    (Math.random() >= 0.5) ? xhr4.open('GET', "json/success.json", true) : xhr4.open('GET', "json/error.json", true);
+    xhr4.send();
+}
+function errorOrSuccess(xhr4) {
+
+    if(xhr4.readyState === 4) {
+        if(xhr4.status === 0) {
+            let result = JSON.parse(xhr4.responseText);
+            let img;
+            let div = document.createElement('div');
+            div.className = "container";
+
+            // Вывод картинки в зависимости от json файла.
+            if(result.result === "error")
+                img = new Image("images/sad.jpg");
+            else
+                img = new Image("images/promo.jpg");
+
+            cont = document.getElementById("myTabContent");
+            container = new PromoContainer("container", img);
+            cont.appendChild(container.render());
+//            document.body.appendChild(container.render());
         }
     }
 }
@@ -131,3 +209,20 @@ if (!xhr) {
 xhr.onreadystatechange = function() {fullMenuContent(xhr)};
 xhr.open('GET', "json/menu.json", true);
 xhr.send();
+
+let xhr4 = false;
+if (window.XMLHttpRequest) {
+    xhr4 = new XMLHttpRequest();
+} else {
+    try {
+        xhr4 = new ActiveXObject('Msxml2.XMLHTTP');
+    } catch(e) {
+        try {
+            xhr4 = new ActiveXObject('Microsoft.XMLHTTP');
+        } catch(e) {}
+    }
+}
+
+if (!xhr4) {
+    alert("Error: It is not possible to create xhr");
+}
